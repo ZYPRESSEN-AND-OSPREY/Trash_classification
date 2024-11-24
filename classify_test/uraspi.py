e32ff91324a0cb3a0d9941111eac354119135ebb
CYPRESSEN <149873042+ZYPRESSEN-AND-OSPREY@users.noreply.github.com>
2024-11-25 02:21:34 +0800

    Update uraspi.py

--- classify_test/uraspi.py
+++ classify_test/uraspi.py
@@ -19,7 +19,7 @@ class GarbageDetectorTFLite:
             self.labels = json.load(f)
         
         # 加载 TFLite 模型并分配张量
-        self.interpreter = tf.lite.Interpreter(model_path=model_path)
+        self.interpreter = tf.lite.Interpreter(model_path=model_path, num_threads=4)
         self.interpreter.allocate_tensors()
         
         # 获取输入和输出张量的信息
