9d5cae869137c0aff22ddc1db559e63a246168fb
CYPRESSEN <149873042+ZYPRESSEN-AND-OSPREY@users.noreply.github.com>
2024-11-25 15:18:30 +0800

    Update dependences.sh

--- dependences.sh
+++ dependences.sh
@@ -2,6 +2,6 @@ conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/main/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/free/
 conda config --add channels https://mirrors.ustc.edu.cn/anaconda/pkgs/r/
 conda config --set show_channel_urls yes
-pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple/
-pip install tensorflow-cpu opencv-python matplotlib numpy pandas scikit-learn
+pip config set global.index-url https://mirrors.ustc.edu.cn/pypi/simple
+pip install tensorflow-cpu tensorflow opencv-python matplotlib numpy pandas scikit-learn
 #一眼丁真，鉴定为纯纯的trash
